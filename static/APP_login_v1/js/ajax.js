fb4bcb1caef2d6291a9c9db2221c68e2795fbfe4
tayer <arnoldincredible@126.com>
2016-11-17 20:37:21 +0800

    重新封装登录相关函数

--- static/APP_login_v1/js/ajax.js
+++ static/APP_login_v1/js/ajax.js
@@ -3,26 +3,20 @@
 by tayir 
 email:arnoldincredible@126.com
 */
-$('#submit').click(function(e){
-    var e = e || window.event
-    e.preventDefault();
-    if(window.event){
-        e.cancelBubble = true;
-    }else{
-        e.stopPropagation();
-    }
 
-    
+$('#submit').on("click",fun_user_login_check);
+
+function fun_user_login_check(e){
+	e.preventDefault(); 
     var action = "login",
-        ischecked = $("#remember_me").is(":checked"),
+        isChecked = $("#remember_me").is(":checked"),
         username = $("#name").val(),
         password = $("#pass").val(),
         datatype = "json";
-        
-    if(!ischecked){
-        REMEMBER_ME = false;
-    }
+        REMEMBER_ME = isChecked?true:false;
+
     var formdata = {"action":action,"username":username,"password":password,"remember_me":REMEMBER_ME,"redirect":REDIRECT};
+
     if(METHOD == "GET"){
         SIGNIN_URL = SIGNIN_URL + "?action=" + action + "$username=" + username + "$password=" + password  + "$remember_me=" + REMEMBER_ME + "$redirect=" + REDIRECT;
         formdata = undefined;
@@ -36,18 +30,18 @@ $('#submit').click(function(e){
                     datatype:datatype,
                     success:function(data){
                         if(data.success){
-                            $("#login-alert").addClass("alert alert-success").html(data.msg);
+                            $("#login-alert").removeClass("alert alert-danger").addClass("alert alert-success").html(data.msg);
                         }else if(!data.success){
-                            $("#login-alert").addClass("alert alert-danger").html(data.msg);
+                            $("#login-alert").removeClass("alert alert-success").addClass("alert alert-danger").html(data.msg);
+                            fun_web_redirect(data.redirect);
                         }
-                        var redirect = data.redirect || REDIRECT;
-                        window.location.href(redirect);
                     }
                 }
 
-    fun_signin(options);
-});
-
-function fun_signin(options){ 
     $.ajax(options);
+}
+
+function fun_web_redirect(redirect){
+	var url = redirect || REDIRECT;
+    window.location.href = url;
 }
